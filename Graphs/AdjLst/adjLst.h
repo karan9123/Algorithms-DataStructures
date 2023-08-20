1b4f22925b44f35eb008de4abfcac1d8d26b9c3c
karan <ka7982@rit.edu>
2023-08-20 16:19:55 +0530

    isConnected with DFS under the hood.

--- Graphs/AdjLst/adjLst.h
+++ Graphs/AdjLst/adjLst.h
@@ -10,18 +10,22 @@
  * @class adjLst
  * @brief Represents an adjacency list for a graph.
  */
-class adjLst {
+class adjLst
+{
 private:
     // Using an unordered_map to store the adjacency list
     std::unordered_map<Node, std::vector<Node>> adjacencyList;
+    int nodes;
 
 public:
     /**
      * @brief Adds a node to the graph.
      * @param node The node to be added.
      */
-    void addNode(const Node& node) {
+    void addNode(const Node &node)
+    {
         adjacencyList[node] = std::vector<Node>();
+        nodes++;
     }
 
     /**
@@ -29,7 +33,8 @@ public:
      * @param fromNode The source node.
      * @param toNode The destination node.
      */
-    void addEdge(const Node& fromNode, const Node& toNode) {
+    void addEdge(const Node &fromNode, const Node &toNode)
+    {
         // Adding the edge to both nodes' adjacency lists
         adjacencyList[fromNode].push_back(toNode);
         adjacencyList[toNode].push_back(fromNode);
@@ -38,18 +43,56 @@ public:
     /**
      * @brief Prints the graph's adjacency list.
      */
-    void printGraph() {
+    void printGraph()
+    {
 
-        for (auto it = adjacencyList.begin(); it != adjacencyList.end(); ++it) {
+        for (auto it = adjacencyList.begin(); it != adjacencyList.end(); ++it)
+        {
             std::cout << "Node " << (it->first).getName() << " is connected to:";
-            for (const Node& neighbor : it->second) {
+            for (const Node &neighbor : it->second)
+            {
                 std::cout << " " << neighbor.getName();
             }
             std::cout << std::endl;
         }
     }
 
-    
+    void isConnected(Node start)
+    {
+        std::unordered_map<Node, bool> visited;
+        for (auto it = adjacencyList.begin(); it != adjacencyList.end(); ++it)
+        {
+            visited[it->first] = false;
+        }
+
+        dfs(start, visited);
+
+        for (auto it = visited.begin(); it != visited.end(); ++it)
+        {
+            std::cout << std::boolalpha;
+            if ((it->first).getName() != start.getName())
+            {
+                std::cout << (it->first).getName() << " is connected to " << start.getName() << " : " << (it->second) << std::endl;
+            }
+        }
+    }
+
+    void dfs(Node start, std::unordered_map<Node, bool> &visited)
+    {
+        if (visited[start])
+        {
+            return;
+        }
+        else
+        {
+            visited[start] = true;
+            std::vector<Node> neighbors = adjacencyList[start];
+            for (const Node &neighbor : neighbors)
+            {
+                dfs(neighbor, visited);
+            }
+        }
+    }
 };
 
 #endif
--- Graphs/AdjLst/main.cpp
+++ Graphs/AdjLst/main.cpp
@@ -5,12 +5,17 @@ int main(){
     Node a("a");
     Node b("b");
     Node c("c");
+    Node d("d");
     adjLst graph;
     graph.addNode(a);
     graph.addNode(b);
     graph.addNode(c);
+    graph.addNode(d);
     graph.addEdge(a,b);
     graph.addEdge(b,c);
     graph.printGraph();
+    graph.isConnected(d);
+
+
     return 0;
 }
